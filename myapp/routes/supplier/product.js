988b52574b9c0b18d5fdf67e2ef7c8a5ffb38e9b
Alice1227 <a23305760@yahoo.com.tw>
2017-03-19 15:18:16 +0800

    106.3.19

--- myapp/routes/supplier/product.js
+++ myapp/routes/supplier/product.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
         res.redirect('/supplierlogin');
     }
 
-    Product.getAll(function(err, productList) {
+    Product.getAll(req.session.supplierm.id, function(err, productList) {
         if (err) {
             next();
         } else {
@@ -40,13 +40,16 @@ router.get('/', function(req, res, next) {
     });
 });
 
-router.post('/add', upload.single('photo'), function(req, res) {
+router.get('/add', function(req, res) {
+    res.render('add');
+});
+
+router.post('/add/save', upload.single('photo'), function(req, res) {
     var tmpPath = req.file.path;
     var albumId = 'fZS2i';
 
     imgur.uploadFile(tmpPath, albumId)
         .then(function(json) {
-            //console.log(json.data.link);
             var newProduct = new Product({
                 name: req.body.name,
                 price: req.body.price,
@@ -69,33 +72,86 @@ router.post('/add', upload.single('photo'), function(req, res) {
         });
 });
 
-router.post('/edit', upload.single('photo'), function(req, res) {
-    var tmpPath = req.file.path;
-    var albumId = 'fZS2i';
-
-    imgur.uploadFile(tmpPath, albumId)
-        .then(function(json) {
-            //console.log(json.data.link);
-            var newProduct = new Product({
-                name: req.body.name,
-                price: req.body.price,
-                description: req.body.description,
-                photo: json.data.link,
-                supplierId: req.session.supplierm.id
-            });
-            newProduct.save(function(err) {
+router.post('/edit', function(req, res) {
+    Product.get(req.body.id, function(err, product) {
+        if (err) {
+            console.log(err);
+        } else {
+            SupplierMember.get(product.supplierId, function(err, supplier) {
                 if (err) {
-                    res.status = err.code;
-                    res.json(err);
+                    console.log(err);
                 } else {
-                    res.redirect('/product');
+                    product.supplier = supplier;
+                    res.render('edit', {
+                        product: product
+                    });
                 }
+            })
+
+        }
+    });
+});
+
+router.post('/edit/save', upload.single('photo'), function(req, res) {
+    if (req.file) {
+        var tmpPath = req.file.path;
+        var albumId = 'fZS2i';
+
+        imgur.uploadFile(tmpPath, albumId)
+            .then(function(json) {
+                var newProduct = new Product({
+                    id: req.body.id,
+                    name: req.body.name,
+                    price: req.body.price,
+                    description: req.body.description,
+                    photo: json.data.link,
+                    supplierId: req.session.supplierm.id
+                });
+                newProduct.save(function(err) {
+                    if (err) {
+                        res.status = err.code;
+                        res.json(err);
+                    } else {
+                        res.redirect('/product');
+                    }
+                });
+                fs.unlink(tmpPath);
+            })
+            .catch(function(err) {
+                console.error(err.message);
             });
-            fs.unlink(tmpPath);
-        })
-        .catch(function(err) {
-            console.error(err.message);
+    } else {
+        var newProduct = new Product({
+            id: req.body.id,
+            name: req.body.name,
+            price: req.body.price,
+            description: req.body.description,
+            photo: req.body.url,
+            supplierId: req.session.supplierm.id
         });
+        newProduct.save(function(err) {
+            if (err) {
+                res.status = err.code;
+                res.json(err);
+            } else {
+                res.redirect('/product');
+            }
+        });
+    }
+});
+
+router.post('/delete', function(req, res) {
+    var newProduct = new Product({
+        id: req.body.id
+    });
+    newProduct.delete(function(err) {
+        if (err) {
+            res.status = err.code;
+            res.json(err);
+        } else {
+            res.redirect('/product');
+        }
+    });
 });
 
 module.exports = router;
--- myapp/routes/supplier/supplierlogin.js
+++ myapp/routes/supplier/supplierlogin.js
@@ -3,15 +3,15 @@ var router = express.Router();
 var SupplierMember = require('../../models/supplierm');
 
 router.get('/', function(req, res, next) {
-    res.render('supplierlogin', {
-        member: null
-    });
+    res.render('supplierlogin');
 });
 
 router.post('/', function(req, res, next) {
     SupplierMember.check(req.body.account, function(err, supplierm) {
         if (req.body.password != supplierm.password) {
-            res.send('Your password is incorrect!');
+            //res.send('Your password is incorrect!');
+            console.log('Your password is incorrect!')
+            res.redirect('/supplierlogin');
         } else {
             req.session.supplierm = supplierm;
             res.redirect('/product');
